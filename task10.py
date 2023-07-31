bb6759e2a89cfec412b13c0dff7a25e88678fe0d
sdrobyshevsky <sdrobyshevsky@mail.ru>
2023-07-31 21:10:12 +0300

    Added info about task10

--- task10.py
+++ task10.py
@@ -3,7 +3,7 @@
 list_1 = [2, -5, 6, 9, 6, 0, 3, -3, 40, 0, 1, 5, 54, 10, 3, 0, -5, 3, 5, -3, 0, -5, -3, 5]
 
 min = 5
-max = 20
+max = 54
 
 for i in range(len(list_1)):
     if min <= list_1[i] <= max:
