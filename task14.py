acf17d9338e84598475acf7606936d5d785a3c3b
sdrobyshevsky <sdrobyshevsky@mail.ru>
2023-08-05 16:35:34 +0300

    Added info about task14

--- task14.py
+++ task14.py
@@ -13,16 +13,6 @@
 # 5 10 15 20 25 30
 # 6 12 18 24 30 36
 
-# def print_operation_table(operation, num_rows, num_columns):
-#     for i in range(1, num_rows + 1):
-#         return_string = ""
-#         for j in range(1, num_columns + 1):
-#             return_string += str(operation(i, j)) + " "
-#         print(return_string.strip())
-
-# print_operation_table(lambda x, y: x * y, 6, 6)
-
-
 def print_operation_table(operation, num_rows=6, num_columns=6):
     a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
     for i in a:
